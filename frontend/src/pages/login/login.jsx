8ae6b06a3c7dc70c98062da9c7c6cdce7e7073bc
ayoub.ch <ay.chaiba98@gmail.com>
2024-01-13 22:13:35 +0100

    add validation form login by useForm and yup

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,17 +1,15 @@
-import  { useEffect, useState } from 'react';
+import  { useEffect , useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { FaSpinner } from "react-icons/fa6";
 import { toast } from 'react-toastify';
 import {useSelector, useDispatch} from 'react-redux';
 import { setLoginOut , setCurrentProfile , setToken } from '../../toolkit/profile';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const Login = () => {
-    let [username, setUsername] = useState('');
-    let [password, setPassword] = useState('');
-    let [loading, setLoading] = useState(false);
-    // let [error, setError] = useState('');
-
     const dispatch = useDispatch()
 
     const {isLogin } = useSelector(state => state.profile);
@@ -23,65 +21,81 @@ const Login = () => {
         isLogin && navigate('/')
     },[isLogin])
 
-    let loginProfile = async (e) => {
-        e.preventDefault();
-        setLoading(true);
+    const schema = yup.object().shape({
+        username: yup.string().required('Username is required').min(4).max(20),
+        password: yup.string().required('Password is required')
+            .min(5, 'Password must be at least 8 characters')
+            .max(20, 'Password must not exceed 20 characters')
+            .matches(
+                /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+                'Password must contain at least one letter, one number, and one special character'
+            ),
+        })
+
+    const { register , handleSubmit , formState : { errors , isValid , isSubmitting  } } = useForm({
+        mode : 'onBlur' ,
+        resolver : yupResolver(schema) ,
+        defaultValues : {
+            username : "" ,
+            password : "" ,
+        }
+    })
+
+
+    let loginProfile = async (data) => {
         try {
-            let response = await axios.post(`${apiUrl}/profile/login`, {
-                username: username,
-                password: password,
-            });
-            let  userData = response.data;
-            sessionStorage.setItem('currentToken', JSON.stringify(userData.token) );
+            let response = await axios.post(`${apiUrl}/profile/login`, data);
+            const  USER_DATA = response.data;
+            sessionStorage.setItem('currentToken', JSON.stringify(USER_DATA.token) );
             dispatch(setLoginOut(true));
-            dispatch(setCurrentProfile(userData.profile));
-            dispatch(setToken(userData.token))
-            toast.success(userData.message, {
+            dispatch(setCurrentProfile(USER_DATA.profile));
+            dispatch(setToken(USER_DATA.token))
+            toast.success(USER_DATA.message, {
                 position: toast.POSITION.BOTTOM_RIGHT
             })
             navigate('/');
         } catch (error) {
-            // setError(error.response.data.message);
             toast.error(error.response.data.message, {
                 position: toast.POSITION.BOTTOM_RIGHT
             })
-        } finally {
-            setLoading(false);
         }
     };
 
     return (
         <div className="flex items-center justify-center h-screen">
-        <form onSubmit={loginProfile} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3">
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-            <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            </div>
+            <p></p>
+            <form onSubmit={handleSubmit(loginProfile)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
+                    <input
+                        className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+                        ${errors.username ? ' border-red-500' : ''}`}
+                        type="text"
+                        placeholder="Username"
+                        {...register('username')}
+                    />
+                    <p className='text-red-500'>{errors.username?.message}</p>
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                        <input
+                            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+                            ${errors.password ? ' border-red-500' : ' '}`}
+                            type="password"
+                            placeholder="Password"
+                            {...register('password')}
+                        />
+                        <p className='text-red-500'>{errors.password?.message}</p>
+                </div>
 
-            <div className="flex items-center justify-between">
-            <button className="flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-            type="submit"
-            disabled={loading}>
-                {loading && <FaSpinner className='animate-spin h-5 w-5 mr-3'/>}
-                {loading ? 'Loding...' : 'Sign In' }
-                </button>
-            </div>
+                <div className="flex items-center justify-between">
+                    <button className="flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                        disabled={ isSubmitting  || !isValid }>
+                        {isSubmitting  && <FaSpinner className='animate-spin h-5 w-5 mr-3'/>}
+                        {isSubmitting  ? 'Loding...' : 'Sign In' }
+                    </button>
+                </div>
             </form>
         </div>
     );
--- frontend/src/pages/register/register.jsx
+++ frontend/src/pages/register/register.jsx
@@ -12,7 +12,10 @@ import * as yup from 'yup';
 const schema = yup.object().shape({
     username: yup.string().min(3).max(15).required('username is required'),
     email: yup.string().email('There are errors in writing an email').required('Email is required'),
-    password: yup.string().min(8).max(20).required('password is required'),
+    password: yup.string().min(8).max(20).required('password is required').matches(
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+        'Password must contain at least one letter, one number, and one special character'
+    ),
 })
 
 const Register = () => {
