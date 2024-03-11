8799dd4f1e8a15722c3bd1660bd4cb2bb30ba651
ayoub.ch <80978382+AyoubChaiba@users.noreply.github.com>
2024-03-11 23:44:14 +0000

    optimze code

--- frontend/src/components/navbar/search/search.jsx
+++ frontend/src/components/navbar/search/search.jsx
@@ -9,10 +9,9 @@ const Search = () => {
     const [tags, setTags] = useState([]);
     const [people, setPeople] = useState([]);
     const [searchItems, setSearchItems] = useState("");
+    const [showSuggestions, setShowSuggestions] = useState(false); // State to track suggestion visibility
 
-
-
-    useEffect(()=>{
+    useEffect(() => {
         const fetchItems = async () => {
             try {
                 const res = await AXIOS_CLIENT.get(`publication/search/test`, {
@@ -26,40 +25,38 @@ const Search = () => {
             } catch (error) {
                 console.log(error);
             }
-        }
-        searchItems.length > 0  &&  fetchItems();
-    },[searchItems])
-
-
+        };
+        searchItems.length > 0 && fetchItems();
+    }, [searchItems]);
 
     const handleChange = async (e) => {
         const searchValue = e.target.value;
-        searchValue >= 0 && setPublications([])
-        searchValue >= 0 && setTags([])
-        searchValue >= 0 && setPeople([])
+        searchValue.length >= 0 && setPublications([]);
+        searchValue.length >= 0 && setTags([]);
+        searchValue.length >= 0 && setPeople([]);
         setSearchItems(searchValue);
-    }
+    };
 
+    const handleToggleSuggestions = () => {
+        setShowSuggestions(!showSuggestions);
+    };
 
     return (
         <div className="search-container">
-        <div className="search">
-            <input
-            type="text"
-            placeholder="Type to search..."
-            value={searchItems}
-            onChange={handleChange}
-            />
-            <FaMagnifyingGlass className="icon-search" />
-        </div>
-        {(publications.length > 0 || tags.length > 0 || people.length > 0) &&
-            <SearchBarList
-                publications={publications}
-                tags={tags}
-                people={people}
+            <div className="search">
+                <input
+                    type="text"
+                    placeholder="Type to search..."
+                    value={searchItems}
+                    onChange={handleChange}
+                    onFocus={() => setShowSuggestions(true)}
+                    onBlur={handleToggleSuggestions}
                 />
-            }
-
+                <FaMagnifyingGlass className="icon-search" />
+            </div>
+            {(showSuggestions && (publications.length >= 0 || tags.length >= 0 || people.length >= 0)) && (
+                <SearchBarList publications={publications} tags={tags} people={people} />
+            )}
         </div>
     );
 };
